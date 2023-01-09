23488b9d1c1467d86ff502b97867e1f778901a95
Markus Haak <markus.haak@posteo.de>
2023-01-10 00:35:43 +0100

    update hyperparemeters

--- tcr_embedding/config_optuna/poe_emb.py
+++ tcr_embedding/config_optuna/poe_emb.py
@@ -1,15 +1,14 @@
 def suggest_params(trial):
     dropout = trial.suggest_float('dropout', 0, 0.3, step=0.05)  # used twice
     activation = trial.suggest_categorical('activation', ['linear', 'leakyrelu'])  # used for conditional sampling
-    rna_hidden = trial.suggest_int('rna_hidden', 500, 2000, step=250)  # hdim should be less than rna_hidden
-    tcr_hidden = trial.suggest_int('tcr_hidden', 500, 2000, step=250)  # hdim should be less than tcr_hidden
+    rna_hidden = trial.suggest_int('rna_hidden', 250, 2000, step=250)  # hdim should be less than rna_hidden
+    tcr_hidden = trial.suggest_int('tcr_hidden', 250, 2000, step=250)  # hdim should be less than tcr_hidden
     hdim = trial.suggest_int('hdim', 100, min(rna_hidden, tcr_hidden, 800), step=100)  # shared_hidden should be less than hdim
     shared_hidden = trial.suggest_int('shared_hidden', 100, min(hdim * 2, 500),
                                       step=100)  # zdim should be less than shared_hidden
     num_layers = trial.suggest_int('num_layers', 1, 3, step=1) if activation == 'leakyrelu' else 1
     rna_num_layers = trial.suggest_int('rna_num_layers', 1, 3, step=1)
     tcr_num_layers = trial.suggest_int('tcr_num_layers', 1, 3, step=1)
-    tfmr_encoding_layers = trial.suggest_int('tfmr_encoding_layers', 1, 4, step=1)  # used twice
     loss_weights_kl = trial.suggest_float('loss_weights_kl', 1e-10, 1e-4, log=True)
     loss_weights_seq = trial.suggest_float('loss_weights_tcr', 1e-5, 1, log=True)
 
